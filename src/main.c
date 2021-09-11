401019595a7856716d85cb06c365e1e086479757
KTheXIII <pratchaya.k.git@gmail.com>
2021-09-11 16:04:12 +0200

    Add Image datatype

--- src/main.c
+++ src/main.c
@@ -8,6 +8,67 @@
 
 #define APPLICATION_TITLE "Conway's Game of Life (SDL2)"
 
+typedef struct {
+    int32_t width;
+    int32_t height;
+    int32_t channels;
+
+    uint8_t* buffer;
+} GOL_Image;
+typedef struct {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+    uint8_t alpha;
+} GOL_Color;
+
+GOL_Image* GOL_ConstructImage(int32_t width, int32_t height, int32_t channels) {
+    GOL_Image* image = (GOL_Image*)malloc(sizeof(GOL_Image));
+    image->width     = width;
+    image->height    = height;
+    image->channels  = channels;
+    image->buffer    = (uint8_t*)malloc(width * height * channels * sizeof(uint8_t));
+    return image;
+}
+void GOL_DestructImage(GOL_Image* image) {
+    free(image->buffer);
+    free(image);
+}
+void GOL_ImageSetPixel(GOL_Image* image,
+                       int32_t x, int32_t y,
+                       uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha) {
+    if (x > -1 && x < image->width && y > -1 && y < image->height) {
+        int32_t index = (y * image->channels) * image->width + (x * image->channels);
+        image->buffer[index + 0] = red;
+        image->buffer[index + 1] = green;
+        image->buffer[index + 2] = blue;
+        image->buffer[index + 3] = alpha;
+    }
+}
+void GOL_ImageSetPixelColor(GOL_Image* image,
+                            int32_t x, int32_t y,
+                            GOL_Color const* color) {
+    if (x > -1 && x < image->width && y > -1 && y < image->height) {
+        int32_t index = (y * image->channels) * image->width + (x * image->channels);
+        image->buffer[index + 0] = color->red;
+        image->buffer[index + 1] = color->green;
+        image->buffer[index + 2] = color->blue;
+        image->buffer[index + 3] = color->alpha;
+    }
+}
+uint8_t* GOL_ImageGetBuffer(GOL_Image* image) {
+    return image->buffer;
+}
+int32_t GOL_ImageGetHeight(GOL_Image* image) {
+    return image->height;
+}
+int32_t GOL_ImageGetWidth(GOL_Image* image) {
+    return image->width;
+}
+int32_t GOL_ImageGetChannels(GOL_Image* image) {
+    return image->channels;
+}
+
 int32_t main(int32_t argc, char const* argv[]) {
     MAYBE_UNUSED(argc);
     MAYBE_UNUSED(argv);
@@ -41,9 +102,25 @@ int32_t main(int32_t argc, char const* argv[]) {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderClear(renderer);
 
-    SDL_Surface* uv_surf = SDL_LoadBMP("assets/uvgrid.bmp");
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, uv_surf);
-    SDL_FreeSurface(uv_surf);
+    //SDL_Surface* uv_surf = SDL_LoadBMP("assets/uvgrid.bmp");
+    //SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, uv_surf);
+    //SDL_FreeSurface(uv_surf);
+
+    int32_t width    = 256;
+    int32_t height   = 256;
+
+    SDL_Texture* texture = SDL_CreateTexture(renderer,
+                                             SDL_PIXELFORMAT_ABGR8888,
+                                             SDL_TEXTUREACCESS_STREAMING,
+                                             width, height);
+    GOL_Image* image = GOL_ConstructImage(width, height, 4);
+
+    for (int32_t i = 0; i < height; i++) {
+        for (int32_t j = 0; j < width; j++) {
+            GOL_ImageSetPixel(image, j, i, 255, 0, 255, 255);
+        }
+    }
+    SDL_UpdateTexture(texture, NULL, GOL_ImageGetBuffer(image), width * 4 * sizeof(uint8_t));
 
     uint8_t is_running = 1;
     while(is_running) {
@@ -79,6 +156,7 @@ int32_t main(int32_t argc, char const* argv[]) {
         SDL_PollEvent(&event);
     }
 
+    GOL_DestructImage(image);
     // Delete texture memory
     SDL_DestroyTexture(texture);
 
